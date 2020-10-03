ed98314866e76a914a30a6c1fad5fbd9b970ba55
togethersUnwise <46249507+togethersUnwise@users.noreply.github.com>
2020-10-03 20:07:57 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+var qs = document.querySelector.bind(document)
+
+let myImage = qs('img');
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/hinata.png') {
+      myImage.setAttribute('src','images/team.png');
+    }
+	else if (mySrc === 'images/team.png') {
+      myImage.setAttribute('src','images/hinata.png');
+    }
+}
+
+let myButton = qs('button')
+let myHeading = qs('h1')
+
+if (! localStorage.getItem('name')) {
+	setUserName();
+} else {
+	let storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Haikyuu is cool, ' + myName;
+}
+function setUserName() {
+	let myName = prompt('There is Virus!. Enter name to proceeds');
+	localStorage.setItem('name', myName);
+	myHeading.textContent = 'Haikyuu is cool, ' + myName;
+}
